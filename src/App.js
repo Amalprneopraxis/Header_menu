89d279b1ff95040ca67235e1efe3d2ac53f882a1
Amalprneopraxis <amal.pr@neopraxis.in>
2024-02-09 15:51:05 +0530

    add reactdom to render the page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import React from 'react';
-import ResponsiveAppBar from './components/megamenu/CouponDrawer';
 import Slider from './components/header/Bannerslide';
+import ReactDOM from 'react-dom';
 
 function App() {
   return (
@@ -17,3 +17,14 @@ function App() {
 }
 
 export default App;
+
+
+// Find the container where you want to insert the button
+const targetContainer = document.getElementById('container-id');
+
+if (targetContainer) {
+  // Render the button component into the target container
+  ReactDOM.render(<Slider />, targetContainer);
+} else {
+  console.error('Target container not found.');
+}
