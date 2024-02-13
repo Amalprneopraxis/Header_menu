41f4ffc3800a445fb9cc047e3a2d569502b24b12
Amalprneopraxis <amal.pr@neopraxis.in>
2024-02-13 12:02:31 +0530

    navigation menu

---
+++ src/Navigationmenu.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Navigationmenu = () => {
+    return (
+        <>
+            <div>
+                <h1>Component 1</h1>
+                <p>This is the first component.</p>
+            </div>
+        </>
+    );
+}
+
+export default Navigationmenu;
